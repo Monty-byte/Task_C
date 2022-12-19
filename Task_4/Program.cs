f9b6c46ab1be83806ac5313d3ea224a9272d2cc0
Anton <kondalov92@gmail.com>
2022-12-19 19:00:48 +0400

    Задача 4 решена

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int number_one = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число: ");
+int number_two = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите третье число: ");
+int number_three = int.Parse(Console.ReadLine() ?? "");
+
+int max = Math.Max(number_one, Math.Max(number_two, number_three));
+
+Console.Write($"{number_one}, {number_two}, {number_three} -> {max};");
+
+
+
