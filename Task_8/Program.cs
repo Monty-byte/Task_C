df4fde105fd11b32b5d9de3b9ecfefc76d850d25
Anton <kondalov92@gmail.com>
2022-12-19 18:34:23 +0400

    Задача 8 решена

---
+++ Task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{number} -> ");
+
+for (int count = 2; count <= number; count ++)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+}
