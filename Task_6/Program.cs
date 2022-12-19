1457313b2e1e46ffc84f972d99a57e7b632e45a4
Anton <kondalov92@gmail.com>
2022-12-19 18:39:33 +0400

    Задача 6 решена

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+if (number % 2 == 0)
+{
+    Console.Write($"{number} -> Да;");
+}
+else
+{
+    Console.Write($"{number} -> Нет;");
+}
