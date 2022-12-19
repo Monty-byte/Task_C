dd3248657991461ad686a82f279585a1af2ff53d
Anton <kondalov92@gmail.com>
2022-12-19 19:13:13 +0400

    Задача 2 решена

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+
+if ( a > b)
+{
+    Console.Write($"a = {a}; b = {b} -> max = {a};");
+}
+else
+{
+    Console.Write($"a = {a}; b = {b} -> max = {b};");
+}
