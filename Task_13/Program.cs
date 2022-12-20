7f8fe03cfb2f6c9ad6e025a2e908cf7124a65767
Anton <kondalov92@gmail.com>
2022-12-21 00:13:10 +0400

    Задача 13 решена

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); //  Преобразует строковое представление числа с указанным основанием системы счисления в эквивалентное ему 32-битовое целое число со знаком.
+
+string numStr = Convert.ToString(number);
+
+int numDigit = numStr.Length;
+
+if (numDigit > 2)
+{
+    Console.WriteLine($"{number} -> {numStr[2]}");
+}
+else
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+
