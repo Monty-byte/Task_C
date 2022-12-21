9f87bef38d440b8ab5d15a74ad654176859e7d4f
Anton <kondalov92@gmail.com>
2022-12-21 23:25:46 +0400

    Задача 15 решена

---
+++ Task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine() ?? "");
+
+
+
+if (day >= 1 && day <= 5)
+{
+    Console.WriteLine($"{day} -> Нет");
+}
+else
+{
+    Console.WriteLine($"{day} -> Да");
+}
