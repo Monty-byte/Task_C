4e2a95a3e2436ff340e2ee4b8bd28fe409211e5f
Anton <kondalov92@gmail.com>
2022-12-25 13:35:18 +0400

    Задача 23 решена

---
+++ Task_23/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{num} -> ");
+
+for (int index = 1; index <= num; index++)
+{
+    int result = (int) Math.Pow(index, 3);
+    Console.Write($"{result}");
+    if ( index < num)
+    {
+        Console.Write(", ");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+}
+
+
