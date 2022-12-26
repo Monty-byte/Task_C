14dadf00c0bfa0405d7b6e9cb082a1cef7322446
Anton <kondalov92@gmail.com>
2022-12-26 17:52:02 +0400

    Задача 19 решена

---
+++ Task_19/Program.cs
@@ -0,0 +1,34 @@
+﻿int num;
+
+while(true)
+{
+    Console.Write("Введите пятизначное число: ");
+
+    try
+    {
+        num = Convert.ToInt32(Console.ReadLine());
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    }
+}
+
+
+int result = num / 1000 / 10; // Нашли первую цифру
+int resultTwo = num / 1000 % 10; // Нашли вторую цифру
+int resultThree = num % 1000 % 100 / 10; // Нашли предпоследнюю цифру
+int resultFour = num % 1000 % 10; // Нашли последнюю цифру
+
+if (result == resultFour)
+{
+    if (resultTwo == resultThree)
+    {
+        Console.Write($"{num} -> Да.");
+    }
+}
+else
+{
+    Console.Write($"{num} -> Нет.");
+}
