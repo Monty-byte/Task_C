086a3be090cbf3b81e92f496b01435ad026173ee
Anton <kondalov92@gmail.com>
2023-01-12 16:29:58 +0400

    Задача 24 решена

---
+++ Task_24/Program.cs
@@ -0,0 +1,24 @@
+﻿int num;
+while (true)
+{
+    Console.Write("Введите целое число: ");
+
+    try
+    {
+        num = Convert.ToInt32(Console.ReadLine());
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    }
+}
+
+int sum = 0;
+for (int i = 1; i <= num; i++ )
+{
+    sum = sum + i;
+}
+Console.Write($"{num} -> {sum};");
+
+
