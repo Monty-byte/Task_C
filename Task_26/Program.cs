442d6e3d9786b8442d5f82c56c9106e46e983b6c
Anton <kondalov92@gmail.com>
2023-01-12 17:28:50 +0400

    Задача 26 решена

---
+++ Task_26/Program.cs
@@ -0,0 +1,20 @@
+﻿int num;
+
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    try
+    {
+        num = Convert.ToInt32(Console.ReadLine());
+        break;
+    }
+    catch(Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    }
+}
+
+string NumStr = Convert.ToString(num);
+int NumLen = NumStr.Length;
+
+Console.Write($"{num} -> {NumLen};");
