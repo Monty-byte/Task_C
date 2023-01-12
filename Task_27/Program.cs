6eba858904ce9852d2a01c4d86c52630df5df53c
Anton <kondalov92@gmail.com>
2023-01-12 18:05:10 +0400

    Задача 27 решена

---
+++ Task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿int num;
+
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    try
+    {
+        num = Convert.ToInt32(Console.ReadLine());
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    }
+}
+
+//int result = Convert.ToString(num).Sum(c => c - '0');
+
+int result = 0;
+int numb = num;
+while (numb != 0)
+{
+    result += numb % 10;
+    numb /= 10;
+}
+
+Console.Write($"{num} -> {result};");
