40bc23a984e4a70cecb09d8f9825c13d0e51be9f
Anton <kondalov92@gmail.com>
2023-01-20 00:09:41 +0400

    Задача 38 решена

---
+++ Task_38/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+double[] array = GetArray(5, 1, 100);
+
+double maxNumber = GetMaxNumber(array);
+double minNumber = GetMinNumber(array);
+
+Console.Write($"[{String.Join(", ", array)}] -> ");
+Console.WriteLine($"{maxNumber - minNumber}");
+
+// Console.WriteLine($"Максимальное число в массиве: {maxNumber}");
+// Console.WriteLine($"Минимальное число в массиве: {minNumber}");
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = Convert.ToDouble(new Random().Next(minValue, maxValue));
+    }
+    return res;
+}
+
+double GetMaxNumber(double[] array)
+{
+    var max = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+
+    return max;
+}
+
+double GetMinNumber(double[] array)
+{
+    var min = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    
+
+    return min;
+}
