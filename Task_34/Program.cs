2e0527517777347b155dcd0a712186006d250e84
Anton <kondalov92@gmail.com>
2023-01-20 00:08:52 +0400

    Задача 34 решена

---
+++ Task_34/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int[] array = GetArray(4, 100, 1000);
+Console.Write($"[{String.Join(", ", array)}] -> ");
+Console.WriteLine(Check(array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+int Check(int[] arr)
+{
+    int kol = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] %2==0) kol++;
+    }
+    return kol;
+}
+
+
