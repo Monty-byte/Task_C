555b612034a57af7a275789e95fd77c75a8161c0
Anton <kondalov92@gmail.com>
2023-01-22 21:58:04 +0400

    Задача 43 решена

---
+++ Task_43/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+*/
+
+Console.Clear();
+
+double b1 = ReadInt("Введите значение b1: ");
+double k1 = ReadInt("Введите значение k1: ");
+double b2 = ReadInt("Введите значение b2: ");
+double k2 = ReadInt("Введите значение k2: ");
+
+double fit = (b1 - b2) / (k2 - k1);
+double two = (k2 * fit) + b2;
+
+Console.Clear();
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({fit}; {two})");
+
+double ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
